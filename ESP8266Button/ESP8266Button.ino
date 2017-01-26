f8d5bfeef1b76679c7845855bc2e3a721c6de3ed
Thijs <thijsbekke@gmail.com>
2017-01-26 19:37:40 +0100

    Initial commit

---
+++ ESP8266Button/ESP8266Button.ino
@@ -0,0 +1,72 @@
+#include <ESP8266WiFi.h>
+
+const char* ssid = "ssid";
+const char* password = "pasword";
+
+const char* host = "host.gateway.com";
+const int httpPort = 80;
+// This is de default url my gateway uses
+String url = "/buy_button/gateway/order.php";
+int pressed = false;
+unsigned long startTime;
+
+#define BUTTONPIN 2
+#define LEDPIN 3
+#define SEND_INTERVAL  1000 * 5// 30S 
+
+void setup() {
+
+  //Starting up
+  delay(10);
+  
+  // prepare GPIO2
+  pinMode(BUTTONPIN, INPUT_PULLUP);
+
+  //Start wifi
+  WiFi.begin(ssid, password);
+  
+  while (WiFi.status() != WL_CONNECTED) {
+    //Wait for wifi too connect
+    delay(500);
+  }
+
+  startTime = millis()+SEND_INTERVAL;
+  // Starting loop, good luck
+}
+
+void loop() {
+
+  int buttonState = digitalRead(BUTTONPIN);
+
+  if (buttonState == LOW && pressed == false) {
+    //  Button is pressed, save it so it don't fire this event again.
+    pressed = true;
+    ButtonPress();
+  }
+}
+
+
+void ButtonPress() {
+     
+  // Use WiFiClient class to create TCP connections
+  WiFiClient client;
+  
+  if (!client.connect(host, httpPort)) {
+    // If client can't connect reset var, so you can try again.
+    pressed = false;
+    return;
+  }
+      
+  // We now create a URI for the request
+  // This will send the request to the server
+  client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+               "Host: " + host + "\r\n" + 
+               "Connection: close\r\n\r\n");
+  delay(10);
+
+  //Reset var to be able to use the button again
+  pressed = false;
+
+  delay(500);
+}
+
---
+++ gateway/order.php
@@ -0,0 +1,93 @@
+<?php
+//The "Public key" or "Api key" can be retrieved in the webshop.
+//This should be the same as the header 'x-public'.
+$publicKey = 'your_public_key';
+
+//The "Secret key" or "Api secret" can be retrieved in the webshop.
+$secretKey = 'ssst_this_is_a_secret';
+
+//The address of your ccv shop
+$domain = 'https://team-thijs.securearea.eu';
+
+//Set the time zone to utc
+date_default_timezone_set('UTC');
+$timeStamp = date('c');
+
+//First collect a set of products,
+
+//The request URI minus the domain name
+$uri = '/api/rest/v1/products';
+
+//Creating the hash
+$hash = hash_hmac('sha512', "$publicKey|GET|$uri||$timeStamp", $secretKey);
+
+$rCurlHandler = curl_init();
+curl_setopt($rCurlHandler, CURLOPT_URL, $domain.$uri);
+curl_setopt($rCurlHandler, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($rCurlHandler, CURLOPT_HTTPHEADER,
+  array(
+      "x-date: ". $timeStamp,
+      "x-hash: ". $hash,
+      "x-public: ". $publicKey
+  )
+);
+
+$sOutput = curl_exec($rCurlHandler);
+curl_close($rCurlHandler);
+
+//Response : a list with objects of products
+$response = json_decode($sOutput);
+
+
+//Random select one.
+if(!isset($response->items)) {
+    header("HTTP/1.0 204 No products found");
+}
+
+$random = rand(0, (count($response->items)-1));
+
+$orderRow = new stdClass();
+$orderRow->product_id = $response->items[$random]->id;
+$orderRow->count = 1;
+
+$order = new stdClass();
+$order->customer = new stdClass();
+//Your own user id
+$order->user_id = 4740289;
+$order->orderrows = array(
+    $orderRow
+);
+
+
+//HTTP method in upppercase (ie: GET, POST, PATCH, DELETE)
+$method = 'POST';
+
+//The request URI minus the domain name
+$uri = '/api/rest/v1/orders';
+
+//Datafields to Json
+$data = json_encode($order);
+
+$hashString = "$publicKey|$method|$uri|$data|$timeStamp";
+$hash = hash_hmac('sha512', $hashString, $secretKey);
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $domain . $uri);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_HTTPHEADER,
+    array(
+        "x-date: " . $timeStamp,
+        "x-hash: " . $hash,
+        "x-public: " . $publicKey
+    )
+);
+
+curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $method);
+
+$sOutput = curl_exec($ch);
+//Response : a product object
+$response = json_decode($sOutput);
+print_r(curl_error($ch));
+curl_close($ch);
